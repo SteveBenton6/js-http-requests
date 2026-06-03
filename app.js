fb52409dfb9751d6ee2c8321c332bd6c1ffbef71
Steve Benton <steve@bentonemail.uk>
2026-06-03 15:17:51 +0100

    update js example formatting

---
+++ app.js
@@ -0,0 +1,27 @@
+// Example 1 -  Intro to AJAX
+console.log("\nSECTION 1 - Intro to AJAX");
+// No Code
+
+// Example 2 -  JSON & XML
+console.log("\nSECTION 2 - JSON & XML");
+
+// Example 3 -  XMLHttpRequests: The Basics
+console.log("\nSECTION 3 - XMLHttpRequests: The Basics");
+
+// Example 4 -  XMLHttpRequests: Chaining Requests
+console.log("\nSECTION 5 - XMLHttpRequests: Chaining Requests");
+
+// Example 5 -  A Better Way: Fetch!
+console.log("\nSECTION 6 - A Better Way: Fetch!");
+
+// Example 6 -  Chaining Fetch Requests
+console.log("\nSECTION 6 - Chaining Fetch Requests");
+
+// Example 7 -  Refactoring Fetch Chains
+console.log("\nSECTION 7 - Refactoring Fetch Chains");
+
+// Example 8 -  An Even Better Way: Axios
+console.log("\nSECTION 8 - An Even Better Way: Axios");
+
+// Example 9 -  Sequential Axios Requests
+console.log("\nSECTION 9 - Sequential Axios Requests");
